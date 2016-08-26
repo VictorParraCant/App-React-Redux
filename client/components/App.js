e04f6751035b55fa4117c47d0c21a9ea4075ae39
Victor Parra <victor.parracant@gmail.com>
2016-08-26 13:05:39 +0200

    React-router setup

--- client/components/App.js
+++ client/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import Greetings from './Greetings.js'
+import NavigationBar from './NavigationBar.js'
 
 class App extends React.Component {
   render() {
     return(
-      <Greetings />
+      <div className="container">
+        <NavigationBar />
+        {this.props.children}
+      </div>
     );
   }
 }
